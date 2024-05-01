5ca59621171f1263dd88da15afe41c07930f3a6e
PratikMane@0112 <pratikmane610@gmail.com>
2024-05-01 14:32:04 +0530

    Add file

---
+++ Over sampled/SVM/RandomForest.R
@@ -0,0 +1,86 @@
+# Install and load necessary packages
+# install.packages("caTools")
+# install.packages("rpart")
+# install.packages("caret")
+
+library(caTools)
+library(rpart)
+library(caret)
+
+# Load the dataset
+# Replace 'your_data.csv' with the actual name of your dataset file
+data <- read.csv("random_over_sampled.csv")
+
+# Define categorical columns
+categorical_columns <- c("Department", "BusinessTravel", "EducationField", 
+                         "Gender", "JobRole", "MaritalStatus", "OverTime")
+
+# Convert "Yes" to 1 and "No" to 0 in the Attrition column
+data$Attrition <- ifelse(data$Attrition == "Yes", 1, 0)
+
+# Convert specified categorical columns to factors
+data[, c("Attrition", categorical_columns)] <- lapply(data[, c("Attrition", categorical_columns)], as.factor)
+# Count rows before outlier handling
+rows_before <- nrow(data)
+# Identify outliers using Z-score method
+outliers <- which(abs(scale(data[, sapply(data, is.numeric)])) > 3, arr.ind = TRUE)
+# Remove outliers if found
+if (length(outliers) > 0) {
+  # Remove outliers
+  data <- data[-outliers, ]} 
+# Count rows after outlier handling
+rows_after <- nrow(data)
+# Print count of rows before and after outlier handling
+cat("Rows before Z-score method of outlier handling:", rows_before, "\n")
+cat("Rows after Z-score method of outlier handling:", rows_after, "\n")
+# Manual scaling normalization
+normalize <- function(x) {
+  return((x - min(x)) / (max(x) - min(x)))}
+# Apply normalization to numerical columns
+numeric_columns <- sapply(data, is.numeric)
+if (sum(numeric_columns) > 0) {
+  data[, numeric_columns] <- lapply(data[, numeric_columns], normalize)
+  
+  cat("\nDataset normalized.\n")
+} else {
+  cat("\nNo numerical columns found in the dataset.\n")
+}
+# Set the seed for reproducibility
+set.seed(123)
+data <- data[sample(nrow(data)), ]
+
+# Split the data into training (70%) and testing (30%) sets
+split <- sample.split(data$Attrition, SplitRatio = 0.75)
+train_data <- subset(data, split == TRUE)
+test_data <- subset(data, split == FALSE)
+
+# Create a training control specifying the 10-fold cross-validation
+train_control <- trainControl(method = "cv", number = 10)
+
+selected_features <- c("MonthlyIncome", "JobRole", "OverTime", "Age", "DailyRate", "HourlyRate", "MonthlyRate", "TotalWorkingYears", "YearsAtCompany", "DistanceFromHome")
+
+# Create an SVM model using 10-fold cross-validation
+svm_model <- train(Attrition ~ ., data = train_data[, c(selected_features, "Attrition")], method = "svmPoly", trControl = train_control)
+
+# Make predictions on the test set
+predictions <- predict(svm_model, newdata = test_data)
+
+# Confusion matrix to evaluate the model
+conf_matrix <- table(predictions, test_data$Attrition)
+print(conf_matrix)
+
+# Calculate accuracy
+accuracy <- sum(diag(conf_matrix)) / sum(conf_matrix)
+print(paste("Accuracy:", accuracy))
+
+# Calculate sensitivity (true positive rate)
+sensitivity <- conf_matrix[2, 2] / sum(conf_matrix[2, ])
+print(paste("Sensitivity:", sensitivity))
+
+# Calculate precision (positive predictive value)
+precision <- conf_matrix[2, 2] / sum(conf_matrix[, 2])
+print(paste("Precision:", precision))
+
+# Calculate specificity (true negative rate)
+specificity <- conf_matrix[1, 1] / sum(conf_matrix[1, ])
+print(paste("Specificity:", specificity))
---
+++ both sampled/Decision Tree/RFE.R
@@ -0,0 +1,79 @@
+library(caTools)
+library(rpart)
+library(caret)
+
+# Load the dataset
+# Replace 'your_data.csv' with the actual name of your dataset file
+data <- read.csv("bothsampled.csv")
+
+# Define categorical columns
+categorical_columns <- c("Department", "BusinessTravel", "EducationField", 
+                         "Gender", "JobRole", "MaritalStatus", "OverTime")
+
+# Convert "Yes" to 1 and "No" to 0 in the Attrition column
+data$Attrition <- ifelse(data$Attrition == "Yes", 1, 0)
+
+# Convert specified categorical columns to factors
+data[, c("Attrition", categorical_columns)] <- lapply(data[, c("Attrition", categorical_columns)], as.factor)
+
+# Count rows before outlier handling
+rows_before <- nrow(data)
+# Identify outliers using Z-score method
+outliers <- which(abs(scale(data[, sapply(data, is.numeric)])) > 3, arr.ind = TRUE)
+# Remove outliers if found
+if (length(outliers) > 0) {
+  # Remove outliers
+  data <- data[-outliers, ]} 
+# Count rows after outlier handling
+rows_after <- nrow(data)
+# Print count of rows before and after outlier handling
+cat("Rows before Z-score method of outlier handling:", rows_before, "\n")
+cat("Rows after Z-score method of outlier handling:", rows_after, "\n")
+# Manual scaling normalization
+normalize <- function(x) {
+  return((x - min(x)) / (max(x) - min(x)))}
+# Apply normalization to numerical columns
+numeric_columns <- sapply(data, is.numeric)
+if (sum(numeric_columns) > 0) {
+  data[, numeric_columns] <- lapply(data[, numeric_columns], normalize)
+  
+  cat("\nDataset normalized.\n")
+} else {
+  cat("\nNo numerical columns found in the dataset.\n")
+}
+set.seed(123) # Set seed for reproducibility
+data <- data[sample(nrow(data)), ]
+
+# Split the data into training (75%) and testing (25%) sets
+split <- sample.split(data$Attrition, SplitRatio = 0.75)
+train_data <- subset(data, split == TRUE)
+test_data <- subset(data, split == FALSE)
+
+# Use the selected_features
+selected_features <- c("OverTime", "JobRole", "HourlyRate", "DailyRate", "MonthlyRate", "MonthlyIncome", "Age", "PercentSalaryHike", "JobSatisfaction", "DistanceFromHome", "StockOptionLevel", "EducationField", "NumCompaniesWorked", "EnvironmentSatisfaction", "RelationshipSatisfaction", "Education", "WorkLifeBalance", "TrainingTimesLastYear", "TotalWorkingYears", "YearsAtCompany", "YearsWithCurrManager", "MaritalStatus", "JobInvolvement", "YearsSinceLastPromotion", "BusinessTravel", "YearsInCurrentRole", "JobLevel", "Gender")
+# Create a training control specifying the 10-fold cross-validation
+train_control <- trainControl(method = "cv", number = 10)
+
+# Create a decision tree model using 10-fold cross-validation
+model <- train(Attrition ~ ., data = train_data[, c(selected_features, "Attrition")], method = "rpart", trControl = train_control)
+
+# Make predictions on the test set
+predictions <- predict(model, newdata = test_data[, c(selected_features, "Attrition")])
+
+# Confusion matrix to evaluate the model
+conf_matrix <- table(predictions, test_data$Attrition)
+print(conf_matrix)
+
+# Calculate evaluation parameters
+# Calculate accuracy
+accuracy <- sum(diag(conf_matrix)) / sum(conf_matrix)
+print(paste("Accuracy:", accuracy))
+# Calculate sensitivity (true positive rate)
+sensitivity <- conf_matrix[2, 2] / sum(conf_matrix[2, ])
+print(paste("Sensitivity:", sensitivity))
+# Calculate precision (positive predictive value)
+precision <- conf_matrix[2, 2] / sum(conf_matrix[, 2])
+print(paste("Precision:", precision))
+# Calculate specificity (true negative rate)
+specificity <- conf_matrix[1, 1] / sum(conf_matrix[1, ])
+print(paste("Specificity:", specificity))
